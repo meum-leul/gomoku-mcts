cd9527493a65f5f241d3b6995de52eb842187dec
박상현 <twodude@naver.com>
2017-11-30 20:56:04 +0900

    Update gomoku.py
    error fixed

--- gomoku.py
+++ gomoku.py
@@ -202,22 +202,22 @@ def UCTPlayGame():
         if state.playerJustMoved == 2:
             rootstate = copy.deepcopy(state)
             m = UCT(rootstate, itermax = iternum)
-    else:
-        while True:
-            # m n
-            try:
-                m, n = input("which Do you want? : ").split()
-                m = int(m);
-                m -= 1
-                n = int(n);
-                n -= 1
-                m = m * sidesize + n
-            except:
-                m = -1
+        else:
+            while True:
+                # m n
+                try:
+                    m, n = input("which Do you want? : ").split()
+                    m = int(m);
+                    m -= 1
+                    n = int(n);
+                    n -= 1
+                    m = m * sidesize + n
+                except:
+                    m = -1
 
-            if m < 0 or m >= mapsize: print("WRONG VALUE")
-            elif state.state[m] != 0: print("WRONG VALUE")
-            else: break
+                if m < 0 or m >= mapsize: print("WRONG VALUE")
+                elif state.state[m] != 0: print("WRONG VALUE")
+                else: break
 
         # print("Best Move : " + str(m) + "\n")
         state.DoMove(m)
